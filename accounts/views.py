efb2f241dacbb4e5a671d417bec9f3b4b3166438
eude313 <eudes.charles@student.moringaschool.com>
2021-08-30 18:57:52 +0300

    add inititial changes

--- accounts/views.py
+++ accounts/views.py
@@ -3,10 +3,22 @@ from django.http import HttpResponse
 
 
 def home(request):
-    return HttpResponse('home page')
+    return render(request, 'account/dashboard.html')
 
 def product(request):
     return HttpResponse('product page')
 
 def customer(request):
     return HttpResponse('customer page')
+
+
+
+"""
+
+---accounts --------------
+-----templates -----------
+-------accounts -----------
+----------dashboard.html---
+----------product.html------
+
+"""
